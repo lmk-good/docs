155a6de5c99b24137c1e152f1a2a650bd750d59a
tjise/SRHW180279 <tjise/SRHW180279>
2020-04-28 20:33:30 +0800

    更新时间

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title:'Personal Notes',
+    themeConfig: {
+      lastUpdated: '上次更新',
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Vue.js', link: '/about/' },
+        { text: 'JavaScript', link: '/about/' },
+        { text: '留言', link: '/about/' },
+        { text: '关于我', link: '/about/' },
+        { text: 'External', link: 'https://google.com' },
+        { text: 'GitHub', link: 'https://google.com' },
+      ]
+    }
+  }
+  
